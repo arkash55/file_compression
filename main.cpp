22f4a5025df7c0d4e91164b6664762928f5b858d
arkash55 <arkash.vijay55@gmail.com>
2025-02-07 15:36:10 +0000

    added print statements with compressed/decompressed file size comparisons

--- main.cpp
+++ main.cpp
@@ -20,19 +20,14 @@ using namespace std;
 
 
 int main() {
+    // Enter file names
     string in_file_name = "notes.txt";
-    // string in_file_name = "test.txt";
     string compressed_file_name = "a_cmp.txt";
     string decompressed_file_name = "a_d_cmp.txt";
-
-
 
     
     HuffmanTree huffman_tree;
     ofstream* compressed_file = huffman_tree.compress_data(in_file_name, compressed_file_name);
-
-
-    //print the size the compressed file
 
 
     ofstream* decompressed_file = huffman_tree.decompress(compressed_file_name, decompressed_file_name);
